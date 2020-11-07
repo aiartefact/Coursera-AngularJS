2a5ba384d1909fd769c3a65693f4020fcd3370e9
aiartefact <71671107+aiartefact@users.noreply.github.com>
2020-11-07 23:06:20 +0200

    Ready

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -6,7 +6,42 @@ angular.module('LunchCheck', [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
-  
+  $scope.lunch = "";
+  $scope.msg = "";
+
+  $scope.lunchProc = function () {
+    if ($scope.lunch == "") //Check for empty input
+      $scope.msg = "Please enter data first";
+    else {
+      var itemArr = $scope.lunch.split(',');
+      var totalItemNum = 0;
+
+      for (var i = 0; i < itemArr.length; i++) { //Counting items
+        if ( !( (itemArr[i] == "") || (!CheckForNonSpaceChar(itemArr[i])) ) ) { //Check for empty items
+          totalItemNum++;
+        }
+      }
+
+      if (totalItemNum == 0)  //Check for output conditions
+        $scope.msg = "You entered a list of empty items";
+      else {
+        if (totalItemNum <= 3)  
+          $scope.msg = "Enjoy!";
+        else
+          $scope.msg = "Too much!";
+      }
+      
+    }
+    
+  };
+}
+
+function CheckForNonSpaceChar (str) {  //Function checks a string for non-space characters. If there are non-space characters it returns "true".
+  for (var i = 0; i < str.length; i++) {
+    if (str[i].charCodeAt() != 32)
+      return true;
+  }
+  return false;
 }
 
 })();
